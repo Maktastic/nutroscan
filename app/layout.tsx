8c890e5b9a45e988f4cfec1e59e90421ba6a7824
Maktastic <mohammadabedin50@gmail.com>
2024-09-30 13:26:46 +0400

    added tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { NextUIProvider } from "@nextui-org/react";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react"
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: "Nutroscan: AI Personalized Meal Plans for Better Health | Custom Diet Plans for Health Conditions",
@@ -18,31 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
 			<Head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6860205400583484"
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6860205400583484"
         crossOrigin="anonymous"></script>
-          <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MPBB4DV6');`,
-          }}
-        />
         <link rel="icon" href="/assets/favicon.ico" />
 			</Head>
+      <GoogleTagManager gtmId="GTM-MPBB4DV6" />
       <body
         className={`antialiased`}
       >
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MPBB4DV6"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <NextUIProvider>
           {children}
           <Analytics />
