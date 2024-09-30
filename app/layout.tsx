77ab44e051819797e4ace9fe500b959bc72c5a01
Maktastic <mohammadabedin50@gmail.com>
2024-09-30 13:38:05 +0400

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,14 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { NextUIProvider } from "@nextui-org/react";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react"
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Nutroscan: AI Personalized Meal Plans for Better Health | Custom Diet Plans for Health Conditions",
   description: "Nutroscan offers AI-generated meal plans tailored to your specific health conditions. Achieve optimal health with our personalized diet solutions.",
 };
 
+const GTM_ID = 'GTM-MPBB4DV6';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,12 +19,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-			<Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6860205400583484"
-        crossOrigin="anonymous"></script>
+      <Head>
         <link rel="icon" href="/assets/favicon.ico" />
-			</Head>
-      <GoogleTagManager gtmId="GTM-MPBB4DV6" />
+      </Head>
+      <Script id='google-adsense' src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6860205400583484" crossOrigin="anonymous" async strategy="afterInteractive">
+      </Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body
         className={`antialiased`}
       >
@@ -31,7 +40,11 @@ export default function RootLayout({
           {children}
           <Analytics />
         </NextUIProvider>
-
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
